8e7dc106c8783b821fc16b631f5d16f2e2b5bf8d
Daniel Givens <mail@danielgivens.com>
2017-04-01 20:49:00 -0400

    SEO

--- docroot/index.php
+++ docroot/index.php
@@ -2,7 +2,33 @@
 <html>
 	<head>
 		<title>Tree Tech</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+		<meta name="author" content="Tree Tech">
+		<meta name="subject" content="Business">
+		<meta name="Description" content="We are your local arborists caring for the urban forest. Estimates and consultations are free so give us a call − we love talking trees! 267-225-3012">
+		<meta name="Classification" content="Tree pruning and removal">
+		<meta name="Keywords" content="tree, arborist, philadelphia, pruning, insured, estimate, removal">
+		<meta name="Geography" content="4045 Ludlow Street, Philadelphia, PA 19104">
+		<meta name="Language" content="English">
+		<meta name="Copyright" content="Tree Tech">
+		<meta name="Designer" content="Daniel Givens">
+		<meta name="distribution" content="Global">
+		<meta name="Robots" content="INDEX,FOLLOW">
+		<meta name="zipcode" content="19104">
+		<meta name="city" content=" Philadelphia">
+		<meta name="country" content="USA">
+		<meta property="og:locale" content="en_US" />
+		<meta property="og:type" content="website" />
+		<meta property="og:description" content="We Make Internet" />
+		<meta property="og:site_name" content="Tree Tech" />
+		<meta property="og:title" content="Tree Tech - Philadelphia Tree Pruning and Removal">
+		<meta property="og:description" content="We are your local arborists caring for the urban forest. Estimates and consultations are free so give us a call − we love talking trees! 267-225-3012">
+		<meta property="og:url" content="http://treetechphila.com">
+		<meta property="og:image" content="http://treetechphila.com/assets/images/fb-4.jpg">
+		<meta property="og:image" content="http://treetechphila.com/assets/images/fb-2.jpg">
+		<meta property="og:image" content="http://treetechphila.com/assets/images/fb-3.jpg">
+		<meta property="og:image" content="http://treetechphila.com/assets/images/fb-5.jpg">
+		<meta property="og:image" content="http://treetechphila.com/assets/images/fb-1.jpg">
+		<meta property="og:image" content="http://treetechphila.com/assets/images/fb-6.jpg">
 		<script src="https://use.typekit.net/tfw0adp.js"></script>
 		<script>try{Typekit.load({ async: true });}catch(e){}</script>
 		<link rel="stylesheet" href="assets/css/style.css" />
@@ -59,33 +85,33 @@
 		<section id="content">
 			<div class="scroller">
 				<article id="pruning">
-					<img src="assets/images/photo-pruning.jpg" />
+					<img src="assets/images/photo-pruning.jpg" alt="Climbing and pruning"  />
 					<h1>Tree Pruning</h1>
 					<p>Trees can be pruned for a variety of reasons, both functional and aesthetic.  Trees can be thinned to allow light to pass through them, or to reduce weight and control growth.  Pruning can also remove dead wood and hanging branches.  Trees can also be reduced and have problematic sections removed.  Often in urban situations trees need to be pruned to clear them from buildings, sidewalks, wires, lights or other trees.  We are also experienced at fine pruning, and can prune large and small trees, and shrubs to achieve the look that you want.</p>
-					<img src="assets/images/photo-suspension.jpg" />
+					<img src="assets/images/photo-suspension.jpg" alt="Suspending branch" />
 				</article>
 				<article id="removal">
 					<h1>Tree Removal</h1>
 					<p>Whether it be from disease, decay, or some other defect trees sometimes need to be removed.  We work hard to save any tree we can, but in cases where the tree could be dangerous to people or property we recommend removal.  When the site allows it we like to work with clients to manage a tree's decline over the end of its life.  In other cases, a tree in decline should be removed sooner since allowing it to continue to decay will only increase the cost and hazards of eventual removal.</p>
 					<p>We specialize in take downs of trees in tight spaces, and will ensure that nothing is damaged and the site is the same when we leave as it was when we found it.</p>
-					<img src="assets/images/photo-removal.jpg" />
+					<img src="assets/images/photo-removal.jpg" alt="Urban tree removal" />
 				</article>
 				<article id="about">
 					<h1>About</h1>
 					<p>TreeTech was founded by Philadelphia native Elias Schewel to provide affordable expert tree pruning and removal services for our clients.  Our shop is a few blocks from the old Tastycake factory, and our office is in West Philly - we are one of the only area tree care companies based in the city.  We are your local arborists caring for the urban forest.  Call us for a quick estimate, local tree emergencies, and efficient, careful service.</p>
 				</article>
 				<article id="sustainability">
-					<img src="assets/images/photo-community.jpg" />
+					<img src="assets/images/photo-community.jpg" alt="Tree climbing rope" />
 					<h1>Sustainability</h1>
 					<p>We are committed to building an environmentally sustainable company. Whenever they are willing we work with our clients to preserve living trees.  If the tree isn't hazardous, we will usually recommend pruning or cabling trees to fix problems that may arise, instead of tree removal.  This preserves the urban tree canopy, and often saves our clients money.</p>
 				</article>
 				<article id="community">
-					<img src="assets/images/photo-urban.jpg" />	
+					<img src="assets/images/photo-urban.jpg" alt="Urban tree pruning" />	
 					<h1>Community</h1>
 					<p>Rooted in our community, we find local uses for all of the wood chips and tree debris that our work produces.  Our wood chips go to <a href=" https://www.bennettcompost.com/" target="_blank">Bennett Compost</a> where it is mixed with household food waste, or chips go to neighborhood gardens and farms.  Larger chunks of wood we produce are turned in Germantown at <a href="http://woodworking.com" target="_blank">Two Trees Woodworking</a>.  When a tree has grown straight, and the site allows it to be felled in one piece, we work with <a href="http://craftworkhome.com" target="_blank">Craftwork Design</a> to transport the log and have it milled in Manayunk.  Then this lumber can be built into custom carpentry at their shop in East Kensington.</p>    
 				</article>
 				<article id="contact">
-					<img src="assets/images/photo-about.jpg" />					
+					<img src="assets/images/photo-about.jpg" alt="Eli Schewel, Philadelphia Urban Arborist" />					
 					<h1>Contact</h1>
 					<ul class="contact">
 						<li><a href="mailto:info@treetechphila.com"><i class="demo-icon icon-mail"></i><span>info@treetechphila.com</span></a></li>
